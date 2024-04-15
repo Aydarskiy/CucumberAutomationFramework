55cd767893fd1750f7a9909af6206ae1bbc2326a
aydarskiy <aydarzarif@gmail.com>
2024-04-14 22:37:12 -0500

    add Magento test

--- src/test/java/utilities/BrowserUtils.java
+++ src/test/java/utilities/BrowserUtils.java
@@ -1,10 +1,8 @@
 package utilities;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,22 +16,23 @@ public class BrowserUtils {
      * This method generates a random email using
      * UUID
      * Example:
-     *        getRandomEmail() -> returns username-123-sdsftw454-fg46@gmail.com
-     *        returns as a String
+     * getRandomEmail() -> returns username-123-sdsftw454-fg46@gmail.com
+     * returns as a String
      */
 
-    public static String getRandomEmail(){
+    public static String getRandomEmail() {
         UUID uuid = UUID.randomUUID();
-        return "username"+uuid+"@gmail.com";
+        return "username" + uuid + "@gmail.com";
     }
 
     /**
      * This method creates a Select object and selects an option by value
+     *
      * @param target
      * @param value
      */
 
-    public static void selectOptionByValue(WebElement target, String value){
+    public static void selectOptionByValue(WebElement target, String value) {
         Select select = new Select(target);
         select.selectByVisibleText(value);
     }
@@ -41,35 +40,60 @@ public class BrowserUtils {
 
     /**
      * This method waits for element to be clickable.
+     *
      * @param webElement
      */
-    public static void waitForElementToBeClickable(WebElement webElement){
+    public static void waitForElementToBeClickable(WebElement webElement) {
 
-        WebDriverWait wait = new  WebDriverWait(Driver.getDriver(), 15);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
         wait.until(ExpectedConditions.elementToBeClickable(webElement));
     }
 
     /**
      * This method waits for text to be present in the webelement.
+     *
      * @param target
      * @param text
      */
-    public static void waitForTextToBePresent(WebElement target, String text){
+    public static void waitForTextToBePresent(WebElement target, String text) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
         wait.until(ExpectedConditions.textToBePresentInElement(target, text));
     }
 
     /**
      * This method takes screenshots of web UI.
+     *
      * @param testName
      */
     public static void takeScreenshot(String testName) throws IOException {
         WebDriver driver = Driver.getDriver();
-        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String path = "src\\test\\resources\\Screenshots\\"+testName+".png";
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = "src\\test\\resources\\Screenshots\\" + testName + ".png";
         File file = new File(path);
-        FileUtils.copyFile(screenshot,file);
+        FileUtils.copyFile(screenshot, file);
+
+    }
 
 
+    /**
+     * This method scrolls the window.
+     *
+     * @param pixels
+     */
+    public static void scrollWindow(int pixels) {
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.driver;
+        jse.executeScript("window.scrollBy(0," + pixels + ")");
+
+    }
+
+
+    /**
+     * this method moves the cursor to target webelement and clicks on it.
+     *
+     * @param target
+     */
+    public static void moveCursorToElement(WebElement target) {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(target).perform();
     }
 }
