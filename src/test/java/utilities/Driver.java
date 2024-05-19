e4a950a96692a8459fae612aa7190e77ff89bac0
aydarskiy <aydarzarif@gmail.com>
2024-05-19 16:08:09 -0500

    add Magento test

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,7 +3,6 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
